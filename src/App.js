837e66d65f6ac7febb48bf03f60ee8fe41390b49
hammamisoumaya <soumayahammami08@gmail.com>
2023-04-14 22:03:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+class MapContainer extends Component {
+  render() {
+    const mapStyles = {
+      width: '100%',
+      height: '100%'
+    };
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: 37.7749,
+         lng: -122.4194
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'YOUR_API_KEY_GOES_HERE'
+})(MapContainer);
